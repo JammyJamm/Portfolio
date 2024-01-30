694abbfdd95b2177e5c08d58d201c4f40ecbde73
SenthamilMunusamy <tamiltanish@gmail.com>
2024-01-30 12:48:41 +0530

    #Landing page animation

---
+++ sourcecode/src/pages/spinner/index.js
@@ -0,0 +1,15 @@
+
+import "./style.scss"
+
+const Spinner = ()=>{
+    return(
+       <div className="ui-spinner">
+        <div className="ui-line">
+            <div className="line"></div>
+        </div>
+            <div className="planFirst"></div>
+            <div className="planSecond"></div>
+          </div>  
+    )
+}
+export default Spinner;
