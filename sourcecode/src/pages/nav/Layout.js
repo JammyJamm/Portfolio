2c4201f3d5c80c3551daa6d87b06723e68ddd174
SenthamilMunusamy <tamiltanish@gmail.com>
2024-02-15 14:08:17 +0530

    #Navigation UI

--- sourcecode/src/pages/nav/Layout.js
+++ sourcecode/src/pages/nav/Layout.js
@@ -7,12 +7,26 @@ const Layout = () => {
         <ul>
           <li>
             <Link to="/">
-              <img src={home} alt="home" />
+              <button class="primary-btn-icon">
+                Home
+                <img src="/static/media/home.50f8698bd0f2a46b0bb3ad09a89f37e8.svg" />
+              </button>
             </Link>
           </li>
           <li>
             <Link to="/contact">
-              <img src={home} alt="contact" />
+              <button class="primary-btn-icon">
+                Contact
+                <img src="/static/media/home.50f8698bd0f2a46b0bb3ad09a89f37e8.svg" />
+              </button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact">
+              <button class="primary-btn-icon">
+                Contact
+                <img src="/static/media/home.50f8698bd0f2a46b0bb3ad09a89f37e8.svg" />
+              </button>
             </Link>
           </li>
         </ul>
